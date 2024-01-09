106a72cccabbc419a9ffe657cdbe7ce688ae8494
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-01-10 00:49:05 +0300

    Finished marketing section

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,14 @@
-const MarketingLayout = ({ children }: {children: React.ReactNode}) => {
-return (<div className="h-full bg-slate-100">
-    <main>{children}</main>
-</div>)
-}aaa
+import { Navbar } from "@/app/(marketing)/_components/navbar";
+import { Footer } from "@/app/(marketing)/_components/footer";
 
-export default MarketingLayout
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
