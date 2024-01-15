44e71aa2b9290237b1da4b2ef8b8671e803fa2f2
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-01-15 21:03:07 +0300

    Workspace sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,10 @@
+import { OrgControl } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control";
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
