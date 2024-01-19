809f1c74aedc0027558c402547533093008f21b0
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-01-20 01:32:26 +0300

    Finished board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
 import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list";
 
 const OrganizationPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
