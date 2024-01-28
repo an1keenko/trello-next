e0b6074e1db6d8a9e04445d1e6d98b8efe1de593
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-01-28 17:40:24 +0100

    Added BoardIdLayout for board routing handling

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
