f5e98bd7ba81b94acf5745c637a98b9bdc4d62d0
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-02-01 23:59:10 +0100

    BoardNavbar

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar..tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar..tsx
@@ -16,8 +16,6 @@ export const BoardNavbar = async ({ id }: BoardNavbarProps) => {
   });
 
   return (
-    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
-      Board NavBar!
-    </div>
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white"></div>
   );
 };
