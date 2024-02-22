ecd455749c81b35bdc52be9c365fc2c16d7fc9d6
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-02-22 15:56:32 +0100

    Created board-title-form.tsx

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar..tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar..tsx
@@ -1,21 +1,14 @@
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
+import { Board } from "@prisma/client";
+import { BoardTitleForm } from "@/app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form";
 
 interface BoardNavbarProps {
-  id: string;
+  data: Board;
 }
 
-export const BoardNavbar = async ({ id }: BoardNavbarProps) => {
-  const { orgId } = auth();
-
-  const board = await db.board.findUnique({
-    where: {
-      id,
-      orgId: orgId!,
-    },
-  });
-
+export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
-    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white"></div>
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
+    </div>
   );
 };
